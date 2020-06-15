76931631ba5c75b689353d2daaa9728498e23676
Tanyo Ivanov <contact@takerman.net>
2020-06-16 02:15:33 +0300

    Use Umbraco instead ASP.NET Core

---
+++ Takerman.Portfolio/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System.Web.Http;
+using System.Web.Mvc;
+using Umbraco.Web.Models;
+using Umbraco.Web.Mvc;
+
+namespace Takerman.Portfolio
+{
+
+    public class HomeController : RenderMvcController
+    {
+        public override ActionResult Index(ContentModel model)
+        {
+            return base.Index(model);
+        }
+
+        public ActionResult Subscribe([FromBody]FormCollection formCollection)
+        {
+            var email = formCollection["email"];
+
+            // TODO: subscribe the email to the news
+
+            return View("Home");
+        }
+    }
+}
