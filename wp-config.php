4d0c2f6d325d586044f613abd04b428996f3ddcc
Tanyo Ivanov <tivanov@takerman.net>
2022-02-01 16:32:37 +0200

    Update the database config

--- wp-config.php
+++ wp-config.php
@@ -26,7 +26,7 @@ define( 'DB_NAME', 'takerman' );
 define( 'DB_USER', 'root' );
 
 /** MySQL database password */
-define( 'DB_PASSWORD', 'Hakerman91!' );
+define( 'DB_PASSWORD', '' );
 
 /** MySQL hostname */
 define( 'DB_HOST', 'localhost' );
