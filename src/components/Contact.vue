cec65e22e47cc565ca0d8ee9ee23f2652377c814
Tanyo Ivanov <tivanov@takerman.net>
2023-02-17 21:53:49 +0200

    port

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -28,14 +28,14 @@
             </div>
 
             <!-- <iframe
-              title="Map"
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2958.134073389703!2d24.760884815327426!3d42.14741767920169!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x14acd10ab6558be7%3A0x8b8db75788fb7b4c!2s862%20101%2C%204000%20Tsentar%2C%20Plovdiv!5e0!3m2!1sen!2sbg!4v1662315803570!5m2!1sen!2sbg"
-              frameBorder="0"
-              style="border: 0, width: '100%' , height: '290px'"
-              allowFullScreen
-              loading="lazy"
-              referrerPolicy="no-referrer-when-downgrade"
-            ></iframe> -->
+                title="Map"
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2958.134073389703!2d24.760884815327426!3d42.14741767920169!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x14acd10ab6558be7%3A0x8b8db75788fb7b4c!2s862%20101%2C%204000%20Tsentar%2C%20Plovdiv!5e0!3m2!1sen!2sbg!4v1662315803570!5m2!1sen!2sbg"
+                frameBorder="0"
+                style="border: 0, width: '100%' , height: '290px'"
+                allowFullScreen
+                loading="lazy"
+                referrerPolicy="no-referrer-when-downgrade"
+              ></iframe> -->
           </div>
         </div>
 
@@ -48,12 +48,7 @@
               </div>
               <div className="form-group col-md-6">
                 <label htmlFor="name">Your Email</label>
-                <input
-                  className="form-control"
-                  name="email"
-                  id="email"
-                  required
-                />
+                <input className="form-control" name="email" id="email" required />
               </div>
               <div className="form-group">
                 <label htmlFor="name">Subject</label>
@@ -61,20 +56,16 @@
               </div>
               <div className="form-group">
                 <label htmlFor="name">Message</label>
-                <textarea
-                  className="form-control"
-                  name="message"
-                  id="message"
-                  rows="10"
-                />
+                <textarea className="form-control" name="message" id="message" rows="10" />
               </div>
               <div className="my-3">
                 <div className="loading">Loading</div>
                 <div className="error-message"></div>
-                <div
-                  className="sent-message"
-                  style="display: isSent ? 'block' : 'none'"
-                >
+                <div className="sent-message">
+                  <!-- <div
+                    className="sent-message"
+                    style="display: isSent ? 'block' : 'none'"
+                  > -->
                   Your message has been sent. Thank you!
                 </div>
               </div>
