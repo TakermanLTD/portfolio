da5fe1fa40092dc82af2f9b8773a5af9cdf24ec2
Tanyo Ivanov <tivanov@takerman.net>
2024-08-29 22:26:17 +0300

    contact

--- Takerman.Portfolio.Server/Controllers/HomeController.cs
+++ Takerman.Portfolio.Server/Controllers/HomeController.cs
@@ -5,15 +5,19 @@ namespace Takerman.Portfolio.Server.Controllers
 {
     [ApiController]
     [Route("[controller]")]
-    public class HomeController(ILogger<HomeController> logger, IMailService mailService) : ControllerBase
+    public class HomeController(ILogger<HomeController> _logger, IMailService _mailService) : ControllerBase
     {
-        private readonly ILogger<HomeController> _logger = logger;
-        private readonly IMailService _mailService = mailService;
-
         [HttpPost("SendMessage")]
         public async Task SendMessage(MailMessageDto message)
         {
-            await _mailService.SendToQueue(message);
+            try
+            {
+                await _mailService.SendToQueue(message);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "An error occured when sending an email.");
+            }
         }
     }
 }
--- takerman.portfolio.client/src/components/Contact.vue
+++ takerman.portfolio.client/src/components/Contact.vue
@@ -1,44 +1,3 @@
-<script>
-export default {
-    data() {
-        return {
-            loading: false,
-            statusClass: 'sent-message',
-            status: null
-        }
-    },
-    methods: {
-        async sendMessage() {
-            this.loading = true;
-            const message = {
-                Subject: document.getElementById('subject').value,
-                From: document.getElementById('email').value,
-                Body: document.getElementById('name').value + ' sent you a message: ' + document.getElementById('message').value,
-                To: 'tivanov@takerman.net'
-            }
-            await fetch('home/sendMessage', {
-                method: 'POST',
-                headers: {
-                    Accept: 'application.json',
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify(message),
-                cache: 'default'
-            }).then((result) => {
-                debugger;
-                this.loading = false;
-                this.statusClass = "sent-message";
-                this.status = 'Your message has been sent. Thank you!';
-            }).catch((error) => {
-                debugger;
-                this.loading = false;
-                this.statusClass = "error-message";
-                this.status = 'There was an error while sending the message! Apologies. Please use the email.';
-            });
-        }
-    },
-}
-</script>
 <template>
     <section id="contact" className="contact">
         <div className="container" data-aos="fade-up">
@@ -52,50 +11,50 @@ export default {
                     <div className="info">
                         <div className="address">
                             <i className="bi bi-geo-alt"></i>
-                            <h4>Location:</h4>
-                            <p>bul. Iztochen, Plovdiv, Bulgaria</p>
+                            <h4>City:</h4>
+                            <p>Plovdiv, Bulgaria</p>
                         </div>
 
                         <div className="email">
                             <i className="bi bi-envelope"></i>
                             <h4>Email:</h4>
-                            <p>contact@takerman.net</p>
+                            <p><a href="mailto:contact@takerman.net">contact@takerman.net</a></p>
                         </div>
 
                         <div className="phone">
                             <i className="bi bi-phone"></i>
                             <h4>Call:</h4>
-                            <p>+447383721742</p>
+                            <p><a href="tel:+447383721742">+447383721742</a></p>
                         </div>
                     </div>
                 </div>
 
                 <div className="col-lg-7 mt-5 mt-lg-0 align-items-stretch">
                     <div id="contactForm" v-once></div>
-                    <form action="#" className="php-email-form">
+                    <form action="#" className="php-email-form" @submit="sendMessage">
                         <div className="row">
                             <div className="form-group col-md-6">
                                 <label htmlFor="name">Your Name</label>
-                                <input className="form-control" id="name" required />
+                                <input className="form-control" id="name" v-model="name" required />
                             </div>
                             <div className="form-group col-md-6">
                                 <label htmlFor="name">Your Email</label>
-                                <input className="form-control" name="email" id="email" required />
+                                <input className="form-control" name="email" id="email" v-model="email" required />
                             </div>
                             <div className="form-group">
                                 <label htmlFor="name">Subject</label>
-                                <input className="form-control" name="subject" id="subject" />
+                                <input className="form-control" name="subject" v-model="subject" id="subject" />
                             </div>
                             <div className="form-group">
                                 <label htmlFor="name">Message</label>
-                                <textarea className="form-control" name="message" id="message" rows="10"></textarea>
+                                <textarea className="form-control" name="message" id="message" v-model="message" rows="10"></textarea>
                             </div>
                             <div className="my-3">
                                 <div v-show="this.loading" className="loading">Loading...</div>
                                 <div v-show="this.status != null" :class="this.statusClass">{{ this.status }}</div>
-                            <br />
+                                <br />
                                 <div className="text-center">
-                                    <button type="button" @click="sendMessage">
+                                    <button type="submit">
                                         Send Message
                                     </button>
                                 </div>
@@ -107,3 +66,54 @@ export default {
         </div>
     </section>
 </template>
+<script>
+export default {
+    data() {
+        return {
+            loading: false,
+            statusClass: 'sent-message',
+            status: null,
+            name: '',
+            email: '',
+            subject: '',
+            message: ''
+        }
+    },
+    methods: {
+        async sendMessage(e) {
+            e.stopPropagation();
+            e.preventDefault();
+
+            if (!this.name || !this.email || !this.message) {
+                alert('Please enter email, name and message');
+                return;
+            }
+
+            this.loading = true;
+            const data = {
+                Subject: this.subject,
+                From: this.email,
+                Body: this.name + ' with email ' + this.email + ' sent you a message: <br /><br />' + this.message,
+                To: 'tivanov@takerman.net'
+            }
+            await fetch('home/sendMessage', {
+                method: 'POST',
+                headers: {
+                    Accept: 'application.json',
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(data),
+                cache: 'default'
+            }).then((result) => {
+                this.loading = false;
+                this.statusClass = "sent-message";
+                this.status = 'Your message has been sent. Thank you!';
+            }).catch((error) => {
+                this.loading = false;
+                this.statusClass = "error-message";
+                this.status = 'There was an error while sending the message! Apologies. Please use the email.';
+            });
+        }
+    },
+}
+</script>
